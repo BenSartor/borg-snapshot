9388bfe861957173ceabad6d62514b5811399e64
Ben Sartor <mail@ben-sartor.de>
2019-01-11 02:47:31 +0100

    create-borg-backup.sh: log interruptions

--- create-borg-backup.sh
+++ create-borg-backup.sh
@@ -5,4 +5,10 @@ set -eu -o pipefail
 declare -rx BORG_PASSPHRASE="Test1234"
 declare -rx BORG_RSH="ssh -i /home/ben/.ssh/id_borgbackup_ed25519"
 
+function interrupted {
+    echo "Backup interrupted"
+}
+trap interrupted INT TERM
+
+
 borg create --stats $(hostname)@backup:test::$(date "+%Y%m%d-%H%M%S") "${HOME}/delme" --exclude "${HOME}/delme/now"
