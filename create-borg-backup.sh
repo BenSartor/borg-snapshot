86a0ec9fae478b21e7468fce2b1486176bf8d23c
Ben Sartor <mail@ben-sartor.de>
2019-01-11 03:01:24 +0100

    create-borg-backup.sh: script to run via cron

--- create-borg-backup.sh
+++ create-borg-backup.sh
@@ -3,7 +3,7 @@
 set -eu -o pipefail
 
 declare -rx BORG_PASSPHRASE="Test1234"
-declare -rx BORG_RSH="ssh -i /home/ben/.ssh/id_borgbackup_ed25519"
+declare -rx BORG_RSH="ssh -i \"${HOME}.ssh/id_borgbackup_ed25519\""
 
 function interrupted {
     echo "Backup interrupted"
