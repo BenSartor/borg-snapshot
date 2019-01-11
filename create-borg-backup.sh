5a875e2c242dc1a9f6b66d6f12ea2bf6c53c7d3c
Ben Sartor <mail@ben-sartor.de>
2019-01-11 03:01:57 +0100

    create-borg-backup.sh: delete old backups

--- create-borg-backup.sh
+++ create-borg-backup.sh
@@ -2,8 +2,11 @@
 
 set -eu -o pipefail
 
+declare -rx BORG_REPO="$(hostname)@backup:test"
 declare -rx BORG_PASSPHRASE="Test1234"
 declare -rx BORG_RSH="ssh -i \"${HOME}.ssh/id_borgbackup_ed25519\""
+
+declare -r TAG_PREFIX="cron-"
 
 function interrupted {
     echo "Backup interrupted"
@@ -11,8 +14,17 @@ function interrupted {
 trap interrupted INT TERM
 
 
+
 borg create\
     --stats\
     --exclude "${HOME}/delme/now"\
-    $(hostname)@backup:test::$(date "+%Y%m%d-%H%M%S")\
+    ::${TAG_PREFIX}$(date "+%Y%m%d-%H%M%S")\
     "${HOME}/delme"
+
+
+borg prune\
+    --list\
+    --prefix "${TAG_PREFIX}"\
+    --keep-daily   7\
+    --keep-weekly  4\
+    --keep-monthly 6
