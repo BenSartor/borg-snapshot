9e1b1232faf098407d9b69baee968cb1ef0fcad3
Ben Sartor <mail@ben-sartor.de>
2019-01-16 23:42:22 +0100

    borg-backup.sh

---
+++ borg-backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+. "$(dirname $(readlink -f $0))/create-borg-backup-settings.sh"
+
+declare -rx BORG_REPO=${BORG_REPO:-"$(hostname)@backup:home-$(whoami)"}
+declare -rx BORG_PASSPHRASE=${BORG_PASSPHRASE:-""}
+declare -rx BORG_RSH=${BORG_RSH:-"ssh -i \"${HOME}/.ssh/id_borgbackup_ed25519\""}
+
+borg $*
+echo "success"
