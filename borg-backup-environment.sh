36790a8d8bd83a4ba2e6c8dc34e75fdbe7bfa71b
Ben Sartor <mail@ben-sartor.de>
2019-01-17 01:11:38 +0100

    borg-backup-environment.sh: use hostname

--- borg-backup-environment.sh
+++ borg-backup-environment.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 
 . "$(dirname $(readlink -f $0))/borg-backup-settings"
 
-declare -rx BORG_REPO=${BORG_REPO:-"$(hostname)@backup:home-$(whoami)"}
+declare -r  HOSTNAME=${HOSTNAME:-$(hostname)}
+declare -rx BORG_REPO=${BORG_REPO:-"${HOSTNAME}@backup:${HOSTNAME}"}
 declare -rx BORG_PASSPHRASE=${BORG_PASSPHRASE:-""}
 declare -rx BORG_RSH=${BORG_RSH:-"ssh -i \"${HOME}/.ssh/id_borgbackup_ed25519\""}
