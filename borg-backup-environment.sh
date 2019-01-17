d9454e0c23a83a8405a40bf04ec27cf32b078ca4
Ben Sartor <mail@ben-sartor.de>
2019-01-17 01:14:38 +0100

    borg-backup-environment.sh: ssh_key

--- borg-backup-environment.sh
+++ borg-backup-environment.sh
@@ -5,6 +5,7 @@ set -eu -o pipefail
 . "$(dirname $(readlink -f $0))/borg-backup-settings"
 
 declare -r  HOSTNAME=${HOSTNAME:-$(hostname)}
+declare -rx SSH_KEY="${HOME}/.ssh/id_borgbackup_ed25519\"
 declare -rx BORG_REPO=${BORG_REPO:-"${HOSTNAME}@backup:${HOSTNAME}"}
 declare -rx BORG_PASSPHRASE=${BORG_PASSPHRASE:-""}
-declare -rx BORG_RSH=${BORG_RSH:-"ssh -i \"${HOME}/.ssh/id_borgbackup_ed25519\""}
+declare -rx BORG_RSH=${BORG_RSH:-"ssh -i \"${SSH_KEY}\""}
