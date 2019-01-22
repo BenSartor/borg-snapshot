eae73a92ff814874926bcddf369cbb1d1e9c2d5c
Ben Sartor <mail@ben-sartor.de>
2019-01-22 23:31:16 +0100

    borg-backup-create.sh: reformat

--- borg-backup-create.sh
+++ borg-backup-create.sh
@@ -11,36 +11,36 @@ function interrupted {
 trap interrupted INT TERM
 
 
-nice -n 19 ionice -c3 "${BORG_BACKUP}" create\
-    --stats\
-    --exclude "/home/*/.cache"\
-    --exclude "/home/*/.gradle"\
-    --exclude "/home/*/.m2"\
-    --exclude "/home/*/.steam/steam/steamapps/"\
-    --exclude "/home/*/.local/share/akonadi"\
-    --exclude "/home/*/Downloads"\
-    --exclude "/var/cache/"\
-    --exclude "/var/tmp/"\
-    --exclude "/var/lib/docker"\
-    --exclude "/var/lib/flatpak"\
-    ::${TAG_PREFIX}$(date "+%Y%m%d-%H%M%S")\
-    /etc\
-    /home\
-    /opt\
-    /root\
-    /srv\
-    /usr\
+nice -n 19 ionice -c3 "${BORG_BACKUP}" create    \
+    --stats                                      \
+    --exclude "/home/*/.cache"                   \
+    --exclude "/home/*/.gradle"                  \
+    --exclude "/home/*/.m2"                      \
+    --exclude "/home/*/.steam/steam/steamapps/"  \
+    --exclude "/home/*/.local/share/akonadi"     \
+    --exclude "/home/*/Downloads"                \
+    --exclude "/var/cache/"                      \
+    --exclude "/var/tmp/"                        \
+    --exclude "/var/lib/docker"                  \
+    --exclude "/var/lib/flatpak"                 \
+    ::${TAG_PREFIX}$(date "+%Y%m%d-%H%M%S")      \
+    /etc                                         \
+    /home                                        \
+    /opt                                         \
+    /root                                        \
+    /srv                                         \
+    /usr                                         \
     /var
 
 
-"${BORG_BACKUP}" prune\
-    --list\
-    --prefix "${TAG_PREFIX}"\
-    --keep-secondly  4\
-    --keep-hourly  6\
-    --keep-daily   7\
-    --keep-weekly  4\
-    --keep-monthly 6
+"${BORG_BACKUP}" prune          \
+    --list                      \
+    --prefix "${TAG_PREFIX}"    \
+    --keep-secondly 4           \
+    --keep-hourly   6           \
+    --keep-daily    7           \
+    --keep-weekly   4           \
+    --keep-monthly  6
 
 "${BORG_BACKUP}" list
 echo "success"
