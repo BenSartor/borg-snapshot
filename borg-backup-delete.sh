34505011bf9ebf2da4e9b46f4e836a3d00a02f6e
Ben Sartor <mail@ben-sartor.de>
2019-01-25 13:47:05 +0100

    borg-backup-delete.sh: deletes user and backups on server

---
+++ borg-backup-delete.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+. "$(dirname $(readlink -f $0))/borg-backup-environment.sh"
+
+
+echo "remove user=${SERVER_USER} on server=${SERVER}"
+ssh root@"${SERVER}" deluser --remove-home "${SERVER_USER}"
+
+echo "remove user's home directory including backups: ${SERVER_USER_HOME}"
+ssh root@"${SERVER}" rm -rf "${SERVER_USER_HOME}"
+
+echo "success"
