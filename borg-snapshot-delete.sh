db85c37ab8e1b1d2fd29ce58f639a44dcfbcb74d
Ben Sartor <mail@ben-sartor.de>
2019-01-25 15:12:33 +0100

    borg-snapshot-delete.sh: improve logs

--- borg-snapshot-delete.sh
+++ borg-snapshot-delete.sh
@@ -15,10 +15,10 @@ fi
 echo "remove user=${SERVER_USER} on server=${SERVER}"
 ssh root@"${SERVER}" deluser --remove-home "${SERVER_USER}"
 
-echo "remove user's home directory including backups: ${SERVER_USER_HOME}"
+echo "remove user's home directory on server including backups: ${SERVER_USER_HOME}"
 ssh root@"${SERVER}" rm -rf "${SERVER_USER_HOME}"
 
-echo "delete ssh key create for borg"
+echo "delete ssh key created for borg"
 rm "${SSH_KEY}"
 rm "${SSH_KEY_PUB}"
 
