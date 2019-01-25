302782e8d18ce295cb080d893114eb2637e326ae
Ben Sartor <mail@ben-sartor.de>
2019-01-25 04:50:36 +0100

    borg-backup-init.sh: do not depend on server scripts anymore

--- borg-backup-environment.sh
+++ borg-backup-environment.sh
@@ -4,9 +4,12 @@ set -eu -o pipefail
 
 . "$(dirname $(readlink -f $0))/borg-backup-settings"
 
-declare -rx SERVER=${SERVER:-""}
-declare -r  HOSTNAME=${HOSTNAME:-$(hostname)}
-declare -rx SSH_KEY=${SSH_KEY:-"/root/.ssh/id_borgbackup_ed25519"}
+declare -r HOSTNAME=${HOSTNAME:-$(hostname)}
+declare -r SERVER=${SERVER:-""}
+declare -r SERVER_BACKUPS_DIRECTORY=${SERVER_BACKUPS_DIRECTORY:-"/srv/backup"}
+declare -r SERVER_USER=${SERVER_USER:-"${HOSTNAME}"}
+declare -r SSH_KEY=${SSH_KEY:-"/root/.ssh/id_borgbackup_ed25519"}
+
 declare -rx BORG_REPO=${BORG_REPO:-"${HOSTNAME}@${SERVER}:${HOSTNAME}"}
 declare -rx BORG_PASSPHRASE=${BORG_PASSPHRASE:-""}
 declare -rx BORG_RSH=${BORG_RSH:-"ssh -i \"${SSH_KEY}\""}
--- borg-backup-init.sh
+++ borg-backup-init.sh
@@ -6,11 +6,27 @@ set -eu -o pipefail
 
 
 ## sshkey
-ssh-keygen -t ed25519 -C "${USER}@${HOSTNAME} backup" -N "" -f "${SSH_KEY}" || echo "using existing ssh key"
+ssh-keygen -t ed25519 -C "${HOSTNAME} backup" -N "" -f "${SSH_KEY}" || echo "using existing ssh key"
+
 
 ## init borg repo on server
-echo "add borg user $(hostname)@${SERVER}"
-ssh root@"${SERVER}" /opt/borg-server/add-borg-user.sh $(hostname) \"$(cat "${SSH_KEY}.pub")\"
+echo "add borg user ${SERVER_USER}@${SERVER}"
+
+declare -r SERVER_USER_HOME="${SERVER_BACKUPS_DIRECTORY}/${SERVER_USER}"
+ssh root@"${SERVER}" adduser --disabled-password --gecos "" --home "${SERVER_USER_HOME}" "${SERVER_USER}" || echo "using existing user: ${SERVER_USER}"
+
+
+declare -r SERVER_USER_BACKUP="${SERVER_USER_HOME}/backup"
+ssh root@"${SERVER}" "mkdir -p \"${SERVER_USER_BACKUP}"\"
+ssh root@"${SERVER}" "mkdir -p \"${SERVER_USER_HOME}/.ssh\""
+
+echo "command=\"cd ${SERVER_USER_BACKUP} ; borg serve --append-only --restrict-to-path ${SERVER_USER_BACKUP}\",restrict $(cat "${SSH_KEY}.pub")" | ssh root@"${SERVER}" "cat > \"${SERVER_USER_HOME}/.ssh/authorized_keys\""
+
+ssh root@"${SERVER}" chown -R "${SERVER_USER}" "${SERVER_USER_HOME}/.ssh/"
+ssh root@"${SERVER}" chgrp -R "${SERVER_USER}" "${SERVER_USER_HOME}/.ssh/"
+ssh root@"${SERVER}" chmod -R og-rx "${SERVER_USER_HOME}/.ssh/"
+
+echo "restricted user on server to borg: ${SERVER_USER}"
 
 
 borg init --encryption=repokey
