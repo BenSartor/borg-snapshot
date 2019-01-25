62d1cbbf1992e92d2e3641fbbfcd4df1544c88d1
Ben Sartor <mail@ben-sartor.de>
2019-01-25 14:09:07 +0100

    borg-backup-delete.sh: delete ssh key created for borg

--- borg-backup-delete.sh
+++ borg-backup-delete.sh
@@ -11,4 +11,8 @@ ssh root@"${SERVER}" deluser --remove-home "${SERVER_USER}"
 echo "remove user's home directory including backups: ${SERVER_USER_HOME}"
 ssh root@"${SERVER}" rm -rf "${SERVER_USER_HOME}"
 
+echo "delete ssh key create for borg"
+rm "${SSH_KEY}"
+rm "${SSH_KEY}.pub"
+
 echo "success"
