72db3b3dd526d8716744beff1d1f7b3d12421046
Ben Sartor <mail@ben-sartor.de>
2019-01-25 04:03:35 +0100

    borg-backup-init.sh: improve rerun ability

--- borg-backup-init.sh
+++ borg-backup-init.sh
@@ -6,10 +6,10 @@ set -eu -o pipefail
 
 
 ## sshkey
-ssh-keygen -t ed25519 -C "${USER}@${HOSTNAME} backup" -N "" -f "${SSH_KEY}"
-
+ssh-keygen -t ed25519 -C "${USER}@${HOSTNAME} backup" -N "" -f "${SSH_KEY}" || echo "using existing ssh key"
 
 ## init borg repo on server
+echo "add borg user $(hostname)@${SERVER}"
 ssh root@"${SERVER}" /opt/borg-server/add-borg-user.sh $(hostname) \"$(cat "${SSH_KEY}.pub")\"
 
 
