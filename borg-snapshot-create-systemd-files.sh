9dd631a0951b2023317436ae7e6c1a9328a34961
Ben Sartor <mail@ben-sartor.de>
2019-01-25 15:00:43 +0100

    borg-snapshot-create-systemd-files.sh: log created files

--- borg-snapshot-create-systemd-files.sh
+++ borg-snapshot-create-systemd-files.sh
@@ -4,9 +4,12 @@ set -eu -o pipefail
 
 declare -r SCRIPT_DIRECTORY=${SCRIPT_DIRECTORY:-$(dirname $(readlink -f $0))}
 declare -r SYSTEMD_DIRECTORY=${SYSTEMD_DIRECTORY:-"/etc/systemd/system"}
+declare -r SYSTEMD_SERVICE=${SYSTEMD_SERVICE:-"${SYSTEMD_DIRECTORY}/borg-snapshot.service"}
+declare -r SYSTEMD_TIMER=${SYSTEMD_TIMER:-"${SYSTEMD_DIRECTORY}/borg-snapshot.timer"}
 declare -r DESCRIPTION=${DESCRIPTION:-"continuous invocation of borg backup"}
 
-cat <<EOF > "${SYSTEMD_DIRECTORY}/borg-snapshot.service"
+echo "create systemd file: ${SYSTEMD_SERVICE}"
+cat <<EOF > "${SYSTEMD_SERVICE}"
 [Unit]
 Description=${DESCRIPTION}
 ConditionACPower=true
@@ -18,7 +21,8 @@ ExecStart=${SCRIPT_DIRECTORY}/borg-snapshot-create.sh
 EOF
 
 
-cat <<EOF > "${SYSTEMD_DIRECTORY}/borg-snapshot.timer"
+echo "create systemd file: ${SYSTEMD_TIMER}"
+cat <<EOF > "${SYSTEMD_TIMER}"
 [Unit]
 Description=${DESCRIPTION}
 
