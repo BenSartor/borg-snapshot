c01ae94028140f4f068da9d6e9273b116d50ddba
Ben Sartor <mail@ben-sartor.de>
2025-08-25 15:11:58 +0200

    borg-snapshot-create.sh: fix ADDITONAL_BACKUP_PATHS

--- borg-snapshot-create.sh
+++ borg-snapshot-create.sh
@@ -34,7 +34,7 @@ nice -n 19 ionice -c3 borg create                \
     --exclude "/tmp"                             \
     --exclude "/lost+found"                      \
     ::${TAG_PREFIX}$(date "+%Y%m%d-%H%M%S")      \
-    / "${ADDITONAL_BACKUP_PATHS}"
+    / ${ADDITONAL_BACKUP_PATHS}
 
 
 borg prune                      \
